1409c089b9985fcb9a8baeddf985544022188c46
Uzair Aftab <71899510+UzairAftab45735@users.noreply.github.com>
2024-10-31 01:23:46 +0500

    Add files via upload

---
+++ ASPNetCore-API-Template/Controllers/GetController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+namespace ASPNetCore_API_Template.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetController : ControllerBase
+    {
+        [HttpGet ("ping")]
+        public IActionResult Ping() 
+        {
+            return Ok("This is a GET API with status code 200.");
+        }
+    }
+}
